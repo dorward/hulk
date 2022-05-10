6e5a23e24e2b5b7d744160f905822fac714f4715
David Dorward <david@dorward.me.uk>
2022-05-10 22:46:16 +0100

    Give unique keys to content elements
    Resolves #5

--- src/store/content.ts
+++ src/store/content.ts
@@ -1,7 +1,11 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+export type Content = {
+	text: string;
+	index: string;
+};
 export interface ContentState {
-	content: string[];
+	content: Content[];
 }
 
 const initialState: ContentState = {
@@ -12,7 +16,7 @@ export const contentSlice = createSlice({
 	name: 'content',
 	initialState,
 	reducers: {
-		add: (state, action: PayloadAction<string>) => {
+		add: (state, action: PayloadAction<Content>) => {
 			state.content.push(action.payload);
 		},
 	},
