bba5380e667bab92155dda4f155d8fefb134bc2f
David Dorward <david@dorward.me.uk>
2022-05-10 23:20:39 +0100

    Add some basic styling
    Resolves #9
    
    It's pretty ugly, but it is early days yet.

---
+++ src/theme/GlobalStyle.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+	background: ${p => p.theme.darkBold};
+	color: ${p => p.theme.lightSoft};
+    font-family: 'Space Mono', monospace;
+  }
+`;
+
+export default GlobalStyle;
