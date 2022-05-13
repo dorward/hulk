a1628a66753aecd4995a3f3c8d4031e8bfd636ce
David Dorward <david@dorward.me.uk>
2022-05-14 00:00:02 +0100

    Clean up some old logging

--- src/ink/inkStory.ts
+++ src/ink/inkStory.ts
@@ -2,8 +2,5 @@ import * as Inkjs from 'inkjs';
 
 import data from '../../hulk.ink.json';
 
-console.log({ data });
-
-console.log({ Inkjs });
 const inkStory = new Inkjs.Story(data);
 export default inkStory;
