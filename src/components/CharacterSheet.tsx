959f167d2be3e29b56cb5403bcc55f5f5ce5a0ad
David Dorward <david@dorward.me.uk>
2022-05-14 12:17:26 +0100

    Refactor attribute display

--- src/components/CharacterSheet.tsx
+++ src/components/CharacterSheet.tsx
@@ -2,21 +2,35 @@ import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 import { RootState } from '../store';
+import { Character } from '../store/reducers';
 
 export const CharacterSheetContainer = styled.div``;
+
+type AttributesProps = Pick<Character, 'attributes'>;
+
+const Attributes = ({ attributes }: AttributesProps) => {
+	return (
+		<table>
+			{Object.entries(attributes)
+				.filter((x) => x[1] > 0)
+				.map(([name, value]) => (
+					<tr>
+						<th scope="row">{name}</th>
+						<td>{value}</td>
+					</tr>
+				))}
+		</table>
+	);
+};
 
 const CharacterSheet = () => {
 	const character = useSelector((state: RootState) => state.state.character);
 	const { attributes } = character;
 	return (
 		<CharacterSheetContainer>
-			{Object.entries(attributes)
-				.filter((x) => x[1] > 0)
-				.map(([name, value]) => (
-					<p>
-						{name}: {value}
-					</p>
-				))}
+			{Object.keys(attributes).length && (
+				<Attributes attributes={attributes} />
+			)}
 		</CharacterSheetContainer>
 	);
 };
