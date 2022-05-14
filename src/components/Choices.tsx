eea1a1e76e6682e72f309f2f679e63586180de65
David Dorward <david@dorward.me.uk>
2022-05-14 12:11:40 +0100

    Style the choices

--- src/components/Choices.tsx
+++ src/components/Choices.tsx
@@ -4,7 +4,27 @@ import styled from 'styled-components';
 import { RootState } from '../store';
 import TextPrompt from './TextPrompt';
 
-export const ChoicesContainer = styled.div``;
+export const ChoicesContainer = styled.ul`
+	list-style: none;
+	text-align: center;
+	margin: 0;
+	padding: 3px 3px 0 3px;
+`;
+
+const Item = styled.li`
+	button,
+	input {
+		border: none;
+		background: ${(p) => p.theme.darkSoft};
+		color: ${(p) => p.theme.lightBold};
+		font-family: 'Space Mono', monospace;
+		padding: 9px;
+		margin: 9px 0;
+		font-size: 1rem;
+		display: block;
+		width: 100%;
+	}
+`;
 
 type Props = {
 	choose(index: number): void;
@@ -15,11 +35,17 @@ const Choices = ({ choose }: Props) => {
 
 	return (
 		<ChoicesContainer>
-			{state.textPrompt && <TextPrompt prompt={state.textPrompt} />}
+			{state.textPrompt && (
+				<Item>
+					<TextPrompt prompt={state.textPrompt} />
+				</Item>
+			)}
 			{state.choices.map((choice) => (
-				<button key={choice.index} onClick={() => choose(choice.index)}>
-					{choice.text}
-				</button>
+				<Item key={choice.index}>
+					<button onClick={() => choose(choice.index)}>
+						{choice.text}
+					</button>
+				</Item>
 			))}
 		</ChoicesContainer>
 	);
